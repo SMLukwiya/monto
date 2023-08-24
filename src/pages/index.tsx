eecb524aa5ac9830e3eb93e9bb622030a7fc064c
danielmontoyo <d.wollendorfer+montoyo@gmail.com>
2023-08-24 10:23:49 +0200

    fix

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,16 +7,23 @@ const Home: NextPage = () => {
   return (
     <>
       <div className="flex h-screen items-center justify-center px-4 text-center sm:px-8 ">
-        <div className="-mt-6 flex max-w-[770px] flex-col items-center justify-center gap-2 text-center">
+        <div className="mt-6 flex max-w-[770px] flex-col items-center justify-center gap-2 text-center">
           <h1 className="text-xl font-semibold leading-tight tracking-tighter">
             Contribute to open source together.
           </h1>
-          <div className="mt-4 flex flex-col gap-2 sm:flex-row">
+          <div className="mt-4 flex flex-col gap-2">
             <Link href="/issues">
               <Button size="sm" variant="secondary" className="text-xs">
                 Get started
               </Button>
             </Link>
+            <a
+              href="https://github.com/montocode/monto"
+              target="_blank"
+              className="pt-8 font-mono text-xs text-slate-500"
+            >
+              GitHub Repo
+            </a>
           </div>
         </div>
       </div>
