a2117c545757efa4a15f8ac7ab17f70652da8ef5
danielmontoyo <d.wollendorfer+montoyo@gmail.com>
2023-08-24 09:50:39 +0200

    fix

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -12,7 +12,7 @@ const MyApp: AppType = ({ Component, pageProps }) => {
   return (
     <>
       <Head>
-        <title>🤙 Contribute to products together</title>
+        <title>Contribute to products together</title>
         <link rel="icon" href="/favicon.ico" />
         <meta property="og:title" content="Monto" />
         <meta
